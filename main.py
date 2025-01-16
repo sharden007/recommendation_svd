6fe39f32b338f45f82f5f5cd75266e738bbaf3e7
Shawn Harden <sharden007@gmail.com>
2025-01-16 11:25:15 -0500

    added datasets

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from surprise import SVD
 from surprise import accuracy
 from surprise.model_selection import train_test_split
 
-# Load the built-in MovieLens 100k dataset
+# Load the built-in MovieLens 100k or 1 million dataset
 # If not already downloaded, it will be automatically fetched
-data = Dataset.load_builtin('ml-100k')
+#data = Dataset.load_builtin('ml-100k')
+data = Dataset.load_builtin('jester')
 
 # Split the dataset into training and testing sets (75% training, 25% testing)
 trainset, testset = train_test_split(data, test_size=0.25)
